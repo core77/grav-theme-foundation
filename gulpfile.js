4769b83b9ce9f3cdee88d0120358551d119f2280
Christian Orendi <chris.orendi@gmail.com>
2017-03-10 10:19:05 +0100

    Making use of browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,27 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concat = require('gulp-concat');
-var watch = require('gulp-watch');
+var browserSync = require('browser-sync').create();
+var concat      = require('gulp-concat');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var watch       = require('gulp-watch');
 
-gulp.task('default', ['sass', 'watch']);
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+      open: false,
+      proxy: "localhost"
+    });
+});
+
+gulp.task('default', ['sass', 'browser-sync', 'watch']);
 
 gulp.task('sass', function () {
   gulp.src('./scss/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(concat('app.css'))
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function () {
