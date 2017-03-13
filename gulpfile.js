a57d391354c2b172dc764b9bc30a24c019408e09
Christian Orendi <chris.orendi@gmail.com>
2017-03-13 10:12:39 +0100

    gulp.watch for changes on twig templates

--- gulpfile.js
+++ gulpfile.js
@@ -26,4 +26,5 @@ gulp.task('sass', function () {
 
 gulp.task('watch', ['browser-sync'], function () {
   gulp.watch('./scss/**/*.scss', ['sass']);
+  gulp.watch('./templates/**/*.twig').on("change", browserSync.reload);
 });
